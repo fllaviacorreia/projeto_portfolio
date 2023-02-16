0851bff0276bb56c9576a52673d93e02b85559ee
Flávia Correia <fllaviacorreia@outlook.com>
2023-02-15 21:43:12 -0300

    update Button Component

--- src/components/elements/button/index.tsx
+++ src/components/elements/button/index.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes } from "react";
 import "./style.css";
 
 type ButtonType = ButtonHTMLAttributes<HTMLButtonElement> & {
