8eebb669a43ae88920d4f4e342e00075dc16a8c1
Flávia Correia <fllaviacorreia@outlook.com>
2023-02-15 21:25:01 -0300

    alter items

---
+++ src/pages/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span>create app</span>
+    </div>
+  );
+}
+
+export default App;
